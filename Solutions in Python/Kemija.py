ec14c8dbecbdafb407f594dddaaad37bac637115
Sureendhar <g.sureendhar@u.nus.edu>
2022-05-14 00:05:49 +0800

    Create Kemija.py

---
+++ Solutions in Python/Kemija.py
@@ -0,0 +1,12 @@
+x=input()
+if 'apa' in x:
+    x=x.replace('apa','a')
+if 'epe' in x:
+    x=x.replace('epe','e')
+if 'upu' in x:
+    x=x.replace('upu','u')
+if 'opo' in x:
+    x=x.replace('opo','o')
+if 'ipi' in x:
+    x=x.replace('ipi','i')
+print(x)
