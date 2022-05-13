6169e1caeb79ea941b653609b79b69d703678a42
Sureendhar <g.sureendhar@u.nus.edu>
2022-05-14 01:26:43 +0800

    Create N-puzzle.py

---
+++ Solutions in Python/N-puzzle.py
@@ -0,0 +1,10 @@
+sum=0
+dict={'A':[0,0],'B':[0,1],'C':[0,2],'D':[0,3],'E':[1,0],'F':[1,1],'G':[1,2],'H':[1,3],'I':[2,0],'J':[2,1],'K':[2,2],'L':[2,3],'M':[3,0],'N':[3,1],'O':[3,2]}
+for i in range(4):
+    x=input()
+    for j in range(4):
+        if x[j]=='.':
+            continue
+        else:
+            sum+=abs(dict[x[j]][0]-i)+abs(dict[x[j]][1]-j)
+print(sum)
