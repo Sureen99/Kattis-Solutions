73a937ef2ed47c9863765f6519cd0117a2bc6ecf
Sureendhar <g.sureendhar@u.nus.edu>
2022-05-31 12:21:54 +0800

    Create StuckInATimeLoop.java

---
+++ Solutions in Java/StuckInATimeLoop.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class time {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int sc = s.nextInt();
+        for (Integer i=1;i<(sc+1);i++) {
+            System.out.println(i.toString()+ " Abracadabra");
+        }
+    }
+}
